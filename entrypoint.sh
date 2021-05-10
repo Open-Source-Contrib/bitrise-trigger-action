1400429ac7b11e7ed6b9ce1e4b8fd8ccb09f0b93
Anas Baadshah <akutty@talabat.com>
2021-05-10 11:19:42 +0400

    added curl request to bitrise and params

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,30 @@
-#!/bin/sh -l
-echo "Running QA workflow for PR created on $3 branch"
-project="https://circleci.com/api/v1.1/project/github/$GITHUB_REPOSITORY/tree/$3"
-echo "Running the $2 job for $project"
-response=$(curl -u $1: -d build_parameters[CIRCLE_JOB]=$2 $project)
+#!/usr/bin/env bash 
+set -euo pipefail
+
+echo "Running workflow for PR created on $4 branch"
+echo "Running the $6 job"
+
+  response=$(curl \
+  -X POST \
+  -H "Authorization: $1" "https://api.bitrise.io/v0.1/apps/$2/builds" \
+  -d "{
+	\"hook_info\": {
+		\"type\": \"bitrise\",
+    \"build_trigger_token\":\"$1\"
+	},
+	\"build_params\": {
+    \"branch\": \"$4\",
+		\"branch_dest\": \"$5\",
+		\"pull_request_id\": \"$7\",
+    \"workflow_id\": \"$6\",
+    \"pull_request_head_branch\":\"$4\",
+    \"branch_repo_owner\":\"talabat-dhme\",
+    \"branch_dest_repo_owner\":\"talabat-dhme\",
+    \"commit_hash\":\"$3\"
+	}
+}")
+
 output=$?
-echo $response
+echo "$response"
 echo ::set-output name=response::$output
 (exit $output)
